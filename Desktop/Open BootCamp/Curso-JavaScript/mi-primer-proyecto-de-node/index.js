4c077788fcab04bfdc68b684677c83fc3fb5b252
Nahuel Rosso <nahuelgrosso25@gmail.com>
2022-11-17 16:08:19 -0300

    First Act

---
+++ Desktop/Open BootCamp/Curso-JavaScript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,12 @@
+// Este es el mensaje de bienvenida
+console.log( "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizaremos los chequeos necesarios
+    Por útino vamos a la ultima linea de saludo
+    Aquí tambien. 
+*/
+
+//Este va a se el mensaje que se envia para despedir
+console.log("Adios");
